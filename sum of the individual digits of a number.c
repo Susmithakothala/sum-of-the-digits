c67c59214a86be4c284e9c8c37fa6e7088aa6005
Susmithakothala <84321100+Susmithakothala@users.noreply.github.com>
2021-08-17 20:06:45 +0530

    Add files via upload

---
+++ sum of the individual digits of a number.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+int main ()
+{
+int n, temp , sum = 0;
+printf (" Enter an integer \n");
+scanf ("%d", &n);
+while (n != 0)
+{
+temp = n % 10;
+sum = sum + temp ;
+n = n / 10;
+}
+printf ("\n Sum of digits = %d ",sum );
+return 0;
+}
